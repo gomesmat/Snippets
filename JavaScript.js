8634fc18355f5dbf02cfa69d1aaf41ef6e5ede2c
Matheus Gomes <matheusribeirogomes@hotmail.com>
2020-01-30 20:43:13 -0300

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,8 +1,28 @@
 // Se objeto é vazio
-
 objVazio = o => {
     if (Object.entries(o).length === 0 && o.constructor === Object) return true;
     else if (o.constructor === Object) return false;
 	else throw "NaO: Parâmetro não é um objeto";
 }
 
+
+// Se o ano (string ou number) é bissexto
+ehBissexto = ano => {
+    let a = 1 * ano;
+    if (a % 4 === 0) {
+      if (a % 100 === 0) {
+        if (a % 400 !== 0) {
+          return false;
+        }
+        if (a % 400 === 0) {
+          return true;
+        }
+      }
+      if (a % 100 !== 0) {
+        return true;
+      }
+    }
+    if (a % 4 !== 0) {
+      return false;
+    }
+  }
