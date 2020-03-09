cc981c9e639fbc0815b655eaedfaab786307ba78
Matheus Gomes <matheusribeirogomes@hotmail.com>
2020-03-09 15:36:06 -0300

    Update bbbc.js

--- bbbc.js
+++ bbbc.js
@@ -1,1 +1,10 @@
-
+let vai = async () => {
+    btVH = await document.getElementsByClassName("_1qOhvoo3qF13WGLsgXWiRn");
+    btVH[2].click();
+    setTimeout(async() => {
+        btVoltar = await document.getElementsByClassName("_3QLPDW1iykGM1e6tm04qJd _1iMqGq8UKv9W1nDF0AvZbu");
+        btVoltar[0].click();
+    }, 2000);  
+    setTimeout(() => { vai() }, 1000)
+}
+vai()
