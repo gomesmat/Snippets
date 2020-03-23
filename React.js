b9192d2a47ac268b517097afd3a34bc206267038
Matheus Gomes <matheusribeirogomes@hotmail.com>
2020-03-23 17:21:22 -0300

    Update React.js

--- React.js
+++ React.js
@@ -3,9 +3,3 @@ handleChange = async event => {
 	await type === "checkbox" ? this.setState({ [name]:checked }) : this.setState({ [name]:value });
 	console.log(this.state)
 }
-
-capitaliza = (s, o) => {
-	if (o === 1) { // capitaliza toda a frase
-		return s.toUpperCase();
-	} else return s.charAt(0).toUpperCase() + s.slice(1);
-};
