34fe03d7860a7e6066a7469acc66445e823c105e
Matheus Gomes <matheusribeirogomes@hotmail.com>
2020-05-24 02:46:35 -0300

    Update AlexaSkillsKit.js

--- AlexaSkillsKit.js
+++ AlexaSkillsKit.js
@@ -4,4 +4,5 @@ let animal = await Alexa.getSlotValue(handlerInput.requestEnvelope, 'animal');
 // Interjeição
 let speakOutput = `Frase normal, <say-as interpret-as="interjection">Bazinga.</say-as>`;
 
+// Ênfase
 let speakOutput = `<emphasis level="strong moderate reduced">não</emphasis>`;
