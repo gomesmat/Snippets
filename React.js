2d5a101edf22e03b1fb893677c73a65d8edfe2b7
Matheus Gomes <matheusribeirogomes@hotmail.com>
2020-10-30 16:15:54 -0300

    Update React.js

--- React.js
+++ React.js
@@ -1,5 +1,20 @@
+// Handle Change
 handleChange = async event => {
 	const {name, value, type, checked} = event.target;
 	await type === "checkbox" ? this.setState({ [name]:checked }) : this.setState({ [name]:value });
-	console.log(this.state)
+}
+
+// Create Ref
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef();  }
+  render() {
+    return (
+      <div>
+        <input type='text' ref={this.myRef} />
+        <button onClick={() => console.log(this.myRef.current.value)}>Console</button>
+      </div>
+    );  
+  }
 }
